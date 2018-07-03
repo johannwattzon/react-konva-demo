44abb5b4bb9c8223b9fe99f20ed3ec4b8c9943e8
Johann Chen <johannchen@Warriors.local>
2018-07-03 11:31:37 -0700

    setup react knova

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import * as React from "react";
+import { Layer, Stage, Text } from "react-konva";
+import ColoredRect from "./ColoredRect";
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text="Try click on rect" />
+          <ColoredRect />
+        </Layer>
+      </Stage>
     );
   }
 }
